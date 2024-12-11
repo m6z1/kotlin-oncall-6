c34a8667ef52a5d0a4699201a7eed5a063fa3ccd
m6z1 <sonmyungj1zz@gmail.com>
2024-12-11 16:13:09 +0900

    feat: 순번이 잘못됐을 때 평일 순번부터 재입력 받기

--- src/main/kotlin/oncall/controller/OnCallController.kt
+++ src/main/kotlin/oncall/controller/OnCallController.kt
@@ -1,5 +1,7 @@
 package oncall.controller
 
+import oncall.model.AssignmentMonth
+import oncall.model.Worker
 import oncall.view.InputView
 
 class OnCallController(
@@ -7,18 +9,50 @@ class OnCallController(
 ) {
 
     fun start() {
-        val assignmentMonth = inputView.readOnCallMonth()
-        val workersOfWeekdays = inputView.readOnCallWeekdays()
-        val workersOfHolidays = inputView.readOnCallHolidays()
+        val month = getOnCallMonth()
+        val (weekdaysWorkers, holidayWorkers) = getWorkers()
 
-        var count = 0
-        workersOfHolidays.forEach { holidaysWorker ->
-            workersOfWeekdays.forEach {
-                if (it.getWorkerNickName() == holidaysWorker.getWorkerNickName()) count++
+    }
+
+    private fun getOnCallMonth(): AssignmentMonth {
+        while (true) {
+            try {
+                val assignmentMonth = inputView.readOnCallMonth()
+                return assignmentMonth
+            } catch (e: Exception) {
+                println(e.message)
             }
         }
-        if (count == workersOfHolidays.size && workersOfHolidays.size == workersOfWeekdays.size) {
+    }
 
+    private fun getWorkers(): Pair<List<Worker>, List<Worker>> {
+        while (true) {
+            try {
+                val weekdaysWorkers = getWorkersOfWeekdays()
+                val holidayWorkers = getWorkersOfHolidays()
+
+                var count = 0
+                holidayWorkers.forEach { holidaysWorker ->
+                    weekdaysWorkers.forEach {
+                        if (it.getWorkerNickName() == holidaysWorker.getWorkerNickName()) count++
+                    }
+                }
+                if (count == holidayWorkers.size && holidayWorkers.size == weekdaysWorkers.size)
+                    return (weekdaysWorkers to holidayWorkers)
+            } catch (e: Exception) {
+                println("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
+                return getWorkers()
+            }
         }
+    }
+
+    private fun getWorkersOfWeekdays(): List<Worker> {
+        val workersOfWeekdays = inputView.readOnCallWeekdays()
+        return workersOfWeekdays
+    }
+
+    private fun getWorkersOfHolidays(): List<Worker> {
+        val workersOfHolidays = inputView.readOnCallHolidays()
+        return workersOfHolidays
     }
 }
